f501f6111eacc411397a7ec3a4479081393b3638
SHAMELESSKING1 <106379133+SHAMELESSKING1@users.noreply.github.com>
2024-03-09 19:20:31 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "WEBHOOK ADDED SUCCESSFULLY FOR PES2UG21CS182\n";
+  cout<<"webhook added"
   return 0;
 }
